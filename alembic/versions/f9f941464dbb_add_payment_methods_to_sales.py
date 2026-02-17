8ee225aa9b4ab5d4d613229888bceb8292c33097
Caio Henrique <cahenromar@gmail.com>
2026-02-17 00:06:25 -0300

    fix(alembic): update down_revision for payment methods migration to reflect correct lineage

--- alembic/versions/f9f941464dbb_add_payment_methods_to_sales.py
+++ alembic/versions/f9f941464dbb_add_payment_methods_to_sales.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = 'f9f941464dbb'
-down_revision: Union[str, None] = '357cfb238719'
+down_revision: Union[str, None] = 'a99a768ecc09'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
